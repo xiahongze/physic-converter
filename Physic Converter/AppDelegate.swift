1831eaaec98cc88ed4f0413a48fe1f1c38fa5fa3
Hongze Xia <hongze.x@gmail.com>
2015-08-22 11:18:00 +1000

    Initial Commit

---
+++ Physic Converter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Physic Converter
+//
+//  Created by Hongze Xia on 22/08/2015.
+//  Copyright (c) 2015 Hongze Xia. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Physic Converter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Physic Converter
+//
+//  Created by Hongze Xia on 22/08/2015.
+//  Copyright (c) 2015 Hongze Xia. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
